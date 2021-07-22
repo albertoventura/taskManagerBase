772660ac4ecf685c94c492499f490f2e7dc3f68b
Alberto Ventura <albertoventura.dev@gmail.com>
2021-07-22 16:50:11 -0300

    versão base de um gerenciador de tarefas para futuras modificações

---
+++ src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { TarefaService,
+         TarefaConcluidaDirective
+       } from './shared';
+
+import { ListarTarefaComponent } from './listar';
+import { EditarTarefaComponent } from './editar';
+import { CadastrarTarefaComponent } from './cadastrar';
+
+
+
+@NgModule({
+  declarations: [
+    ListarTarefaComponent,
+    CadastrarTarefaComponent,
+    EditarTarefaComponent,
+    TarefaConcluidaDirective
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule
+  ],
+  providers: [
+    TarefaService
+  ]
+})
+export class TarefasModule { }
